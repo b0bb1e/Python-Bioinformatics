861812aa8ea035823498c0f83763886c64f35f53
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-06-13 10:04:24 -0700

    Found issue and documented

--- finding/frequency_finder.py
+++ finding/frequency_finder.py
@@ -1,30 +1,44 @@
 BASES = ('A', 'C', 'G', 'T')
 
 def DNA_to_num(DNA: str) -> int:
+    """Converts a DNA string to a number
+
+    For all strings of a certain length, lower numbers will correspond
+    to an earlier point in the alphabet. Numbers are re-used in-between
+    lengths.
+
+    :param DNA: the DNA string of ACGT to convert
+    :type DNA: str
+    :returns: a number corresponding to the DNA string
+    :rtype: int
+    """
+    
     num = 0
     chars = len(DNA)
-    print(chars)
     for i in range(chars):
-        print(DNA[i])
         try:
+            # earlier chars are given more weight/value in the number
             num += BASES.index(DNA[i]) * (4 ** (chars - i - 1))
         except ValueError:
             print('Invalid base "' + DNA[i] + '" is ignored')
-        print(num)
     return num
 
 def num_to_DNA(num: int, chars: int) -> str:
+    """Converts a number to a DNA string
+
+    :param num: the number to convert
+    :type num: int
+    :param chars: the length of the eventual string
+    :type chars: int
+    :returns: the DNA string of length chars equivalent to num
+    :rtype: str
+    """
+    
     DNA = ''
     for i in range(chars):
-        print(BASES[int(num % 4)])
+        # the smaller numbers, pulled off first, go in the back
         DNA = BASES[int(num % 4)] + DNA
         num = num // 4
-        print(num)
+    if num != 0:
+        raise ValueError('Number is too large for length given')
     return DNA
-
-if __name__ == '__main__':
-    pats = ('A', 'C', 'G', 'T', 'AA', 'AC', 'AG', 'AT', 'CA', 'CC', 'CG', 'CT',
-            'GA', 'GC', 'GG', 'GT', 'TA', 'TC', 'TG', 'TT')
-    for pat in pats:
-        print(DNA_to_num(pat))
-    
