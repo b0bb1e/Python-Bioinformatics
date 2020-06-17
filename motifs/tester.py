8bd23f661c128e302a08b9f10cade9f888e006f9
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-06-17 11:18:26 -0700

    Added more test cases

--- motifs/tester.py
+++ motifs/tester.py
@@ -2,6 +2,9 @@ import motifs
 import unittest
 
 class Tester(unittest.TestCase):
+    """Unit tester for motifs.py"""
+
+    # test cases for brute_finder
     known_brute = ((['ATTTGGC', 'TGCCTTA', 'CGGTATC', 'GAAAATT'], 3, 1,
                     {'ATA', 'ATT', 'GTT', 'TTT'}),
                    (['ACGT', 'ACGT', 'ACGT'], 3, 0, {'ACG', 'CGT'}),
@@ -9,7 +12,13 @@ class Tester(unittest.TestCase):
                     {'AAA', 'AAC', 'AAG', 'AAT', 'ACA', 'AGA', 'ATA', 'CAA',
                      'GAA', 'TAA'}),
                    (['AAAAA', 'AAAAA', 'AACAA'], 3, 0, set()),
-                   (['AACAA', 'AAAAA', 'AAaAA'], 3, 0, set()))
+                   (['AACAA', 'AAAAA', 'AACAA'], 3, 0, set()))
+
+    # test cases for median_string
+    known_median = ((['AAATTGACGCAT', 'GACGACCACGTT', 'CGTCAGCGCCTG',
+                      'GCTGAGCACCGG', 'AGTACGGGACAG'], 3, {'ACG', 'GAC'}),
+                    (['ACGT', 'ACGT', 'ACGT'], 3, {'ACG', 'CGT'}),
+                    (['ATA', 'ACA', 'AGA', 'AAT', 'AAC'], 3, {'AAA'}))
 
     def test_brute(self):
         """Brute-force motif finder should find exact motifs"""
@@ -37,6 +46,24 @@ class Tester(unittest.TestCase):
                           ['ACGT'], 3, 0)
         self.assertRaises(ValueError, motifs.brute_finder,
                           [], 3, 0)
+
+    def test_median(self):
+        """Median string finder should find a valid best string"""
+
+        for DNAs, pat_len, corrects in self.known_median:
+            result = motifs.median_string(DNAs, pat_len)
+            self.assertIn(result, corrects)
+
+    def test_median_failure(self):
+        """Median string finder should error on bad input"""
+
+        self.assertRaises(ValueError, motifs.median_string,
+                          ['ACGT', 'ACGT', 'ACGT'], 0)
+        self.assertRaises(ValueError, motifs.median_string, [], 3)
+        self.assertRaises(ValueError, motifs.median_string,
+                          ['ACGT', 'ACGT', ''], 3)
+        self.assertRaises(ValueError, motifs.median_string,
+                          ['ACGT'], 3)
 
 if __name__ == '__main__':
     unittest.main()
