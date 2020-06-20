5928681b9ac13d1693972ad80f9e3f73564f7172
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-06-20 16:52:59 -0700

    Added overlap_graph

--- assembly/assembler.py
+++ assembly/assembler.py
@@ -30,18 +30,52 @@ def path_to_DNA(path: list) -> str:
 
     if not path:
         raise ValueError('Cannot convert nonexistant path')
-    sub_len = len(path[0])
-    for sub_str in path:
-        if len(sub_str) != sub_len:
+    pat_len = len(path[0])
+    for pat in path:
+        if len(pat) != pat_len:
             raise ValueError('All substrings in path must be the same length')
     DNA = path[0][:-1]
-    for sub_str in path:
-        DNA += sub_str[-1]
+    for pat in path:
+        DNA += pat[-1]
     return DNA
+
+def overlap_graph(pats: list) -> dict:
+    """Constructs an overlap graph for a group of patterns
+
+    Patterns directionally overlap if one's headless string matches
+    the other's tailless one
+
+    :param pats: a group of substrings
+    :type pats: list (of strs)
+    :returns: the overlap graph in dictionary form
+    :rtype: dict (strs: lists (of strs))
+    """
+
+    if not pats:
+        raise ValueError('Cannot convert nonexistant pattern')
+    pat_len = len(pats[0])
+    for pat in pats:
+        if len(pat) != pat_len:
+            raise ValueError('All patterns must be the same length')
+    num_pats = len(pats)
+
+    overlap = {}
+    for start in range(num_pats):
+        start_pat = pats[start]
+        for end in range(num_pats):
+            if start != end and start_pat[1:] == pats[end][:-1]:
+                if not start_pat in overlap:
+                    overlap[start_pat] = []
+                overlap[start_pat].append(pats[end])
+    return overlap
 
 if __name__ == '__main__':
     with open('data.txt') as data:
-        path = []
+        pats = []
         for line in data:
-            path.append(line.rstrip())
-    print(path_to_DNA(path))
+            pats.append(line.rstrip())
+    overlap = overlap_graph(pats)
+    with open('output.txt', mode='w') as output:
+        for start in overlap:
+            for end in overlap[start]:
+                output.write('{0} -> {1}\n'.format(start, end))
