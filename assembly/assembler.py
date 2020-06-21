7749d667bde60bbf560e0cb5a7b6d295080be83f
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-06-21 16:07:58 -0700

    Added self_overlap_graph
    self_overlap_graph constructs a DeBruijn graph, but I feel that self-overlap is a clearer explanation of what it does

--- assembly/assembler.py
+++ assembly/assembler.py
@@ -16,8 +16,23 @@ def comp(DNA: str, pat_len: int) -> list:
     DNA_len = len(DNA)
     if DNA_len < pat_len:
         raise ValueError('No substrings of that length')
-
+    
     return sorted([DNA[i:i + pat_len] for i in range(DNA_len - pat_len + 1)])
+
+def verify_pats(pats: list):
+    """Verify that a group of pattens is valid; raises errors
+
+    :param pats: a group of patterns
+    :type pats: list (of strs)
+    :raises: ValueError (if the patterns are invalid)
+    """
+
+    if not pats:
+        raise ValueError('Cannot convert nonexistant pattern')
+    pat_len = len(pats[0])
+    for pat in pats:
+        if len(pat) != pat_len:
+            raise ValueError('All patterns must be the same length')
 
 def path_to_DNA(path: list) -> str:
     """Convert a path of substrings to a condensed string
@@ -28,12 +43,7 @@ def path_to_DNA(path: list) -> str:
     :rtype: str
     """
 
-    if not path:
-        raise ValueError('Cannot convert nonexistant path')
-    pat_len = len(path[0])
-    for pat in path:
-        if len(pat) != pat_len:
-            raise ValueError('All substrings in path must be the same length')
+    verify_pats(path)
     DNA = path[0][:-1]
     for pat in path:
         DNA += pat[-1]
@@ -51,14 +61,8 @@ def overlap_graph(pats: list) -> dict:
     :rtype: dict (strs: lists (of strs))
     """
 
-    if not pats:
-        raise ValueError('Cannot convert nonexistant pattern')
-    pat_len = len(pats[0])
-    for pat in pats:
-        if len(pat) != pat_len:
-            raise ValueError('All patterns must be the same length')
+    verify_pats(pats)
     num_pats = len(pats)
-
     overlap = {}
     for start in range(num_pats):
         start_pat = pats[start]
@@ -69,13 +73,38 @@ def overlap_graph(pats: list) -> dict:
                 overlap[start_pat].append(pats[end])
     return overlap
 
+def self_overlap_graph(pats: list):
+    """Construct a self-overlap graph for a group of patterns
+
+    The tailess bit of a sting self-overlaps with the headless bit
+    
+    :param pats: a group of substrings
+    :type pats: list (of strs)
+    :returns: the overlap graph in dictionary form
+    :rtype: dict (strs: lists (of strs))
+    """
+
+    verify_pats(pats)
+    graph = {}
+    for pat in pats:
+        start = pat[:-1]
+        if not start in graph:
+            graph[start] = []
+        graph[start].append(pat[1:])
+    return graph
+
 if __name__ == '__main__':
     with open('data.txt') as data:
-        pats = []
+        pats_ = []
         for line in data:
-            pats.append(line.rstrip())
-    overlap = overlap_graph(pats)
+            pats_.append(line.rstrip())
+    graph = self_overlap_graph(pats=pats_)
     with open('output.txt', mode='w') as output:
-        for start in overlap:
-            for end in overlap[start]:
-                output.write('{0} -> {1}\n'.format(start, end))
+        for start in graph:
+            output.write('{0} -> '.format(start))
+            num_ends = len(graph[start])
+            for end in range(num_ends):
+                output.write(graph[start][end])
+                if end < num_ends - 1:
+                    output.write(',')
+            output.write('\n')
