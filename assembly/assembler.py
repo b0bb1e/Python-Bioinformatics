8c3102d42854f7aa4f155ae1fb9d992926f96d43
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-06-22 16:41:14 -0700

    Can now assemble strings from substrings!
    Quite a lot of sub-functions added to make this possible. assemble() required graph_to_path, which required graph_to_cycle. Also added a universal binary string generator because why not?

--- assembly/assembler.py
+++ assembly/assembler.py
@@ -73,7 +73,7 @@ def overlap_graph(pats: list) -> dict:
                 overlap[start_pat].append(pats[end])
     return overlap
 
-def self_overlap_graph(pats: list):
+def self_overlap_graph(pats: list) -> dict:
     """Construct a self-overlap graph for a group of patterns
 
     The tailess bit of a sting self-overlaps with the headless bit
@@ -93,18 +93,154 @@ def self_overlap_graph(pats: list):
         graph[start].append(pat[1:])
     return graph
 
+def new_cycle(graph: dict, old_cycle: list=None) -> list:
+    """Find a new cycle within a graph
+
+    :param graph: a self-overlap graph with left-over edges
+    :type graph: dict (strs: lists (of strs))
+    :param old_cycle: a previously-constructed cycle (default None)
+    :type old_cycle: list (of strs)
+    :returns: a new cycle guarenteed to be longer than old_cycle
+    :rtype: list (of strs)
+    """
+    
+    if old_cycle:
+        # find a node with unused edges
+        for node in range(len(old_cycle)):
+            if old_cycle[node] in graph:
+                end = node
+                break
+        # rotate cycle to start & end with this node
+        try:
+            cycle = old_cycle[end:] + old_cycle[1:end + 1]
+            cur = cycle[0]
+        except NameError:
+            raise ValueError('Graph has a closed cycle')
+    else:
+        cur = next(iter(graph.keys()))
+        cycle = []
+
+    # while there are unused edges from the current node
+    while cur in graph:
+        # use an edge
+        cycle.append(graph[cur].pop(-1))
+        # delete nodes from graph if no edges are left
+        if not graph[cur]:
+            del graph[cur]
+        cur = cycle[-1]
+    # force a cycle if there wasn't one before
+    if not old_cycle:
+        cycle.append(cycle[0])
+    return cycle
+
+def graph_to_cycle(graph: dict) -> list:
+    """Constructs a Eulerian cycle from a self-overlap graph
+
+    :param graph: a self-overlap graph
+    :type graph: dict (strs: lists (of strs))
+    :returns: a cycle using all paths in graph
+    :rtype: list
+    """
+
+    cycle = new_cycle(graph)
+    while graph:
+        cycle = new_cycle(graph, cycle)
+    return cycle
+
+def graph_to_path(graph: dict) -> list:
+    """Constructs a Eulerian path from a self-overlap graph
+
+    :param graph: a self-overlap graph
+    :type graph: dict (strs: lists (of strs))
+    :returns: a path using all paths in graph
+    :rtype: list
+    """
+
+    # used to find the start (1 extra out) and end (1 extra in) nodes
+    net_outs = {}
+    for start in graph:
+        if start in net_outs:
+            net_outs[start] += len(graph[start])
+        else:
+            net_outs[start] = len(graph[start])
+        for out in graph[start]:
+            if out in net_outs:
+                net_outs[out] -= 1
+            else:
+                net_outs[out] = -1
+    for node in net_outs:
+        if net_outs[node] == -1:
+            end = node
+        elif net_outs[node] == 1:
+            start = node
+        elif net_outs[node] != 0:
+            raise ValueError('Given graph is not a Eulerian path')
+    # connect end to start to make a cycle
+    if end in graph:
+        graph[end].append(start)
+    else:
+        graph[end] = [start]
+    cycle = graph_to_cycle(graph)
+    start_indexes = []
+    for node in range(len(cycle)):
+        if cycle[node] == start:
+            start_indexes.append(node)
+    # rotate cycle so that start & end appear in proper spots
+    for index in start_indexes:
+        path = cycle[index:] + cycle[1:index + 1]
+        if path[-2] == end:
+            # get rid of the edge between end and start
+            del path[-1]
+            break
+    return path
+
+def assemble(pats: list) -> str:
+    """Assemble a string from substrings
+
+    :param pats: a group of substrings
+    :type pats: list (of strs)
+    :returns: the assembled string
+    :rtype: str
+    """
+
+    verify_pats(pats)
+    graph = self_overlap_graph(pats)
+    path = graph_to_path(graph)
+    return path_to_DNA(path)
+
+def all_binary(bin_len: int) -> list:
+    """Find all binary strings of a certain length
+
+    :param bin_len: the length the binary strings should be
+    :type bin_len: int
+    :returns: all the possible binarys strings of this length
+    :rtype: list (of strs)
+    """
+    
+    if bin_len == 0:
+        return ['']
+    elif bin_len < 0:
+        raise ValueError('Binary strings cannot have negative length')
+    all_bin = []
+    # extend all shorter strings by 0 and 1
+    for short_bin in all_binary(bin_len - 1):
+        all_bin += [short_bin + '0', short_bin + '1']
+    return all_bin
+
+def universal_binary(bin_len: int) -> str:
+    """Constuct a ciruclar universal binary string
+
+    :param bin_len: the length of the universal binary patterns
+    :type bin_len: int
+    :returns: a string containing all binary strings of legnth bin_len
+    :rtype: str
+    """
+    
+    graph = self_overlap_graph(all_binary(bin_len))
+    cycle = graph_to_cycle(graph)
+    # delete last bin_len - 1 chars since this is CIRCULAR
+    cycle = cycle[:-(bin_len - 1)]
+    return path_to_DNA(cycle)
+
 if __name__ == '__main__':
-    with open('data.txt') as data:
-        pats_ = []
-        for line in data:
-            pats_.append(line.rstrip())
-    graph = self_overlap_graph(pats=pats_)
-    with open('output.txt', mode='w') as output:
-        for start in graph:
-            output.write('{0} -> '.format(start))
-            num_ends = len(graph[start])
-            for end in range(num_ends):
-                output.write(graph[start][end])
-                if end < num_ends - 1:
-                    output.write(',')
-            output.write('\n')
+    print(universal_binary(9))
