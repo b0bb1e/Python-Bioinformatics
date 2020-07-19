a27a7b3ba40b58fa7e35bec150d4e5a76dcf0a60
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-07-19 15:01:31 -0700

    Added some test cases

--- sequencing/tester.py
+++ sequencing/tester.py
@@ -20,6 +20,16 @@ class Tester(unittest.TestCase):
                          [[186, 128, 113], [186, 113, 128], [128, 186, 113],
                           [128, 113, 186], [113, 186, 128], [113, 128, 186]]),)
 
+    known_score = (('NQEL', [0, 99, 113, 114, 128, 227, 257, 299, 355, 356,
+                             370, 371, 484], 11),)
+
+    known_leaderboard = (([0, 71, 113, 129, 147, 200, 218, 260, 313, 331, 347,
+                           389, 460,], 10, [113, 147, 71, 129]),)
+
+    known_convolution = (([57, 57, 71, 99, 129, 137, 170, 186, 194, 208, 228,
+                           265, 285, 299, 307, 323, 356, 364, 394, 422, 493],
+                          60, 20, [99, 71, 137, 57, 72, 57]),)
+
     def test_RNA_sequencer(self):
         """RNA sequencer should work correctly"""
     
@@ -53,7 +63,32 @@ class Tester(unittest.TestCase):
 
         for spect, peptides in self.known_sequencing:
             result = spectrum.sequence(spect)
-            self.assertEqual(result, spect)
+            self.assertEqual(result, peptides)
+
+    def test_score(self):
+        """Peptide scoreer should work correctly"""
+
+        for peptide, spect, score in self.known_score:
+            result = spectrum.score_amino(peptide, spect, True)
+            self.assertEqual(result, score)
+
+    def test_leaderboard(self):
+        """Leaderboard sequencer should work correctly"""
+
+        for spect, keep, peptide in self.known_leaderboard:
+            result = spectrum.leaderboard_sequence(spect, keep)
+            # checks that the lists are cyclic-equal
+            self.assertIn(' '.join(map(str, result)),
+                          ' '.join(map(str, peptide * 2)))
+
+    def test_convolution(self):
+        """Convolution sequencer should work correctly"""
+
+        for spect, keep, num_acids, peptide in self.known_convolution:
+            result = spectrum.convolution_sequence(spect, keep, num_acids)
+            # checks that the lists are cyclic-equal
+            self.assertIn(' '.join(map(str, result)),
+                          ' '.join(map(str, peptide * 2)))
 
 if __name__ == '__main__':
     unittest.main()
