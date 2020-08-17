711347d60afadd687ebbba3461f05f67e29b9cf6
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-08-16 17:10:40 -0700

    Completely changing approach
    Now functional instead of class-based, and finds optimal vals while building grid

--- alignment/protein_aligner.py
+++ alignment/protein_aligner.py
@@ -1,70 +1,64 @@
-import dag
+def calc_grid(one: str, two: str, score_matrix: dict,
+                   indel_penalty: int) -> list:
+    grid = [(0, None)]
+    one_len = len(one)
+    two_len = len(two)
+    cur = 0
+    # change to list of lists of tuples, instead of list of tuples
+    # tuples can be (val, row, col) instead of (val, index)
+    for one_i in range(-1, one_len):
+        for two_i in range(-1, two_len):
+            grid.append((float('-inf'), None))
+            on_left_edge = cur % (one_len + 1) == 0
+            on_top_row = cur < one_len + 1
+            
+            if not on_left_edge:
+                val_if_used = grid[cur - 1][0] + indel_penalty
+                if val_if_used > grid[cur][0]:
+                    grid[cur] = (val_if_used, cur - 1)
+            if not on_top_row:
+                val_if_used = grid[cur - (one_len + 1)][0] + indel_penalty
+                if val_if_used > grid[cur][0]:
+                    grid[cur] = (val_if_used, cur - (one_len + 1))
+            if (not on_left_edge) and (not on_top_row):
+                val_if_used = (grid[cur - 1 - (one_len + 1)][0] +
+                               score_matrix[one[one_i]][two[two_i]])
+                if val_if_used > grid[cur][0]:
+                    grid[cur] = (val_if_used, cur - 1 - (one_len + 1))
+            cur += 1
+    print(grid)
+    return grid
 
-class ProteinAligner(dag.DAG):
-    def __init__(self, one: str, two: str, score_matrix: dict,
-                 indel_penalty: int):
-        """Initialize paths using scoring matrix
+def backtrack_alignment(one: str, two: str, grid: list) -> list:
+    alignment = ['', '']
+    one_len = len(one)
+    two_len = len(two)
+    one_i = one_len - 1
+    two_i = two_len - 1
+    cur = (one_len + 1) * (two_len + 1) - 1
+    while cur != 0:
+        diff = cur - grid[cur][1]
+        if diff == 1:
+            alignment[0] += one[one_i]
+            alignment[1] += '-'
+            one_i -= 1
+        elif diff == one_len + 1:
+            alignment[0] += '-'
+            alignment[1] += two[two_i]
+            two_i -= 1
+        elif diff == one_len + 2:
+            alignment[0] += one[one_i]
+            alignment[1] += two[two_i]
+            one_i -= 1
+            two_i -= 1
+        cur = grid[cur][1]
+    return alignment
 
-        :param one: the protein that runs along the 'top'
-        :type one: str
-        :param two: the protein that runs along the 'side'
-        :type two: str
-        :param score_matrix: a protein scoring matrix
-        :type score_matrix: dict (strs: dicts (strs: ints))
-        :param indel_penalty: the penalty for using an indel in alignment
-        :type indel_penalty: int (negative)
-        """
-
-        one_len = len(one)
-        two_len = len(two)
-
-        def node_id(one_i: int, two_i: int) -> int:
-            return (one_i + 1) + ((two_i + 1) * (one_len + 1))
-        paths = {0: {}}
-        for i in range(one_len):
-            cur_id = node_id(i, two_len - 1)
-            paths[cur_id] = {cur_id + 1: indel_penalty}
-        for i in range(two_len):
-            cur_id = node_id(one_len - 1, i)
-            paths[cur_id] = {cur_id + (one_len + 1): indel_penalty}
-        for one_i in range(-1, one_len - 1):
-            for two_i in range(-1, two_len -1):
-                cur_id = node_id(one_i, two_i)
-                paths[cur_id] = {}
-                paths[cur_id][cur_id +1] = indel_penalty
-                paths[cur_id][cur_id + (one_len + 1)] = indel_penalty
-                paths[cur_id][cur_id + 1 + (one_len + 1)] = (
-                    score_matrix[one[one_i + 1]][two[two_i + 1]])
-        self._one = one
-        self._two = two
-        super().__init__(0, ((one_len + 1) * (two_len + 1)) - 1, paths)
-        print('finished set-up')
-    
-    def align(self) -> (int, list):
-        score, path = super().longest_path()
-        print('finished path-finding')
-        alignment = ['', '']
-        last = path[0]
-        one_i = 0
-        two_i = 0
-        one_len = len(self._one)
-        for node in path:
-            diff = node - last
-            if diff == 1:
-                alignment[0] += self._one[one_i]
-                alignment[1] += '-'
-                one_i += 1
-            elif diff == one_len + 1:
-                alignment[0] += '-'
-                alignment[1] += self._two[two_i]
-                two_i += 1
-            elif diff == one_len + 2:
-                alignment[0] += self._one[one_i]
-                alignment[1] += self._two[two_i]
-                one_i += 1
-                two_i += 1
-            last = node
-        return score, alignment
+def align(one: str, two: str, score_matrix: dict,
+          indel_penalty: int) -> (int, list):
+    grid = calc_grid(one, two, score_matrix, indel_penalty)
+    score = grid[(len(one) + 1) * (len(two) + 1) - 1][0]
+    return score, backtrack_alignment(one, two, grid)
 
 def read_score_matrix(file_name: str) -> dict:
     with open(file_name) as score_file:
