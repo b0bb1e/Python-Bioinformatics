16f4edce283258caf4b7221d0b8db43a71f51fc0
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2020-08-20 17:01:38 -0700

    Added more test cases

--- alignment/tester.py
+++ alignment/tester.py
@@ -1,6 +1,6 @@
 import dynamic_practice
 import dag
-import protein_aligner
+import aligner
 import unittest
 
 class Tester(unittest.TestCase):
@@ -17,6 +17,14 @@ class Tester(unittest.TestCase):
                   9, [0, 2, 3, 4]),)
 
     known_align = (('PA', 'APA', 6), ('PLEASANTLY', 'MEANLY', 8))
+
+    known_local = (('MEANLY', 'PENALTY', 15),)
+
+    known_edit = (('MAP', 'CARP', 2), ('PLEASANTLY', 'MEANLY', 5))
+
+    known_fitting = (('GTAGGCTTAAGGTTA', 'TAGATA', 2),)
+
+    known_overlap = (('PAWHEAE', 'HEAGAWGHEE', 1),)
 
     def test_coins(self):
         """Test min-change finder"""
@@ -47,11 +55,34 @@ class Tester(unittest.TestCase):
     def test_align(self):
         """Test global aligner"""
         for one, two, score in self.known_align:
-            score_matrix = protein_aligner.read_score_matrix('blossom.txt')
-            result_s, result_a = protein_aligner.align(one, two,
-                                                       score_matrix, -5)
+            score_matrix = aligner.read_score_matrix('blossom.txt')
+            result_s, result_a = aligner.global_align(one, two, score_matrix)
             self.assertEqual(result_s, score)
-            print(result_a)
+
+    def test_local(self):
+        """Test local aligner"""
+        for one, two, score in self.known_local:
+            score_matrix = aligner.read_score_matrix('pam.txt')
+            result_s, result_a = aligner.local_align(one, two, score_matrix)
+            self.assertEqual(result_s, score)
+
+    def test_edit(self):
+        """Test edit distance calculator"""
+        for one, two, distance in self.known_edit:
+            result = aligner.find_edit_distance(one, two)
+            self.assertEqual(result, distance)
+
+    def test_fitting(self):
+        """Test fitting aligner"""
+        for long, short, score in self.known_fitting:
+            result_s, result_a = aligner.fitting_align(long, short)
+            self.assertEqual(result_s, score)
+
+    def test_overlap(self):
+        """Test overlap aligner"""
+        for before, after, score in self.known_overlap:
+            result_s, result_a = aligner.overlap_align(before, after)
+            self.assertEqual(result_s, score)
 
 if __name__ == '__main__':
     unittest.main()
